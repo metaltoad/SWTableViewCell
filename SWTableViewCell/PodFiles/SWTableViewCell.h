4faeae3102def2cfd61e173ef611a18055e1b424
matthewcraven <matthewcraven@metaltoad.com>
2016-04-08 15:30:39 -0700

    exposing the cell scroll view

--- SWTableViewCell/PodFiles/SWTableViewCell.h
+++ SWTableViewCell/PodFiles/SWTableViewCell.h
@@ -41,6 +41,7 @@ typedef NS_ENUM(NSInteger, SWCellState)
 @property (nonatomic, copy) NSArray *leftUtilityButtons;
 @property (nonatomic, copy) NSArray *rightUtilityButtons;
 @property (nonatomic, strong) SWUtilityButtonView *leftUtilityButtonsView, *rightUtilityButtonsView;
+@property (nonatomic, strong) UIScrollView *cellScrollView;
 
 @property (nonatomic, weak) id <SWTableViewCellDelegate> delegate;
 
@@ -49,6 +50,7 @@ typedef NS_ENUM(NSInteger, SWCellState)
 - (void)hideUtilityButtonsAnimated:(BOOL)animated;
 - (void)showLeftUtilityButtonsAnimated:(BOOL)animated;
 - (void)showRightUtilityButtonsAnimated:(BOOL)animated;
+- (CGPoint)contentOffsetForCellState:(SWCellState)state;
 
 - (BOOL)isUtilityButtonsHidden;
 
--- SWTableViewCell/PodFiles/SWTableViewCell.m
+++ SWTableViewCell/PodFiles/SWTableViewCell.m
@@ -23,7 +23,6 @@ static NSString * const kTableViewCellContentView = @"UITableViewCellContentView
 @property (nonatomic, assign) SWCellState cellState; // The state of the cell within the scroll view, can be left, right or middle
 @property (nonatomic, assign) CGFloat additionalRightPadding;
 
-@property (nonatomic, strong) UIScrollView *cellScrollView;
 @property (nonatomic, strong) UIView *leftUtilityClipView, *rightUtilityClipView;
 @property (nonatomic, strong) NSLayoutConstraint *leftUtilityClipConstraint, *rightUtilityClipConstraint;
 
